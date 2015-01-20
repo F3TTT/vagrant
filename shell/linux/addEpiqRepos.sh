eafc3cd01b856708c658da18602666b2a626a186
F3TTT <ddoughty@epiqsystems.com>
2015-01-20 13:06:36 -0600

    change to yum repolist to revise changes in repo files

--- shell/linux/addEpiqRepos.sh
+++ shell/linux/addEpiqRepos.sh
@@ -3,4 +3,4 @@ sudo cp /home/vagrant/etc/rhel_6_3_X64_local.repo /etc/yum.repos.d
 sudo cp /home/vagrant/etc/rhel_6_latest_X64_local.repo /etc/yum.repos.d
 sudo chown root:root /etc/yum.repos.d/*
 sudo chmod 644 /etc/yum.repos.d/*
-sudo yum --enablerepo=rhel6_u3_base_local,rhel6_addons,rhel6_latest_base_local
+sudo yum repolist
